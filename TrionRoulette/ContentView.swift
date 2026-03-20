f1711b3c051c8cf35bbe9c7541ce2277a5c7b1d6
Leuchten <susumu.hirasawa247@gmail.com>
2026-03-21 00:47:00 +0900

    Initial Commit

---
+++ TrionRoulette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrionRoulette
+//
+//  Created by Torimaru on 2026/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrionRoulette/TrionRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrionRouletteApp.swift
+//  TrionRoulette
+//
+//  Created by Torimaru on 2026/03/21.
+//
+
+import SwiftUI
+
+@main
+struct TrionRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
